0c247026900a74c942e5ef9a6c4c058ebf4ae6bc
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 13:51:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+  
+  def initialize(name)
+    @name=name
+  end
+  
+  def match(list)
+    list.select do|word|
+    @name.split("").sort==word.split("").sort
+    end
+  end
+  
+end
+  
